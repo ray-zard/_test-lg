4618131e02ede96734ea28c51a508bb1e6bb6ae4
RayZard <ray-zard@mail.ru>
2016-08-17 01:33:06 +0400

    подустал

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+var gulp = require ('gulp'),
+	watch = require ('gulp-watch'),
+	preFixer = require ('gulp-autoprefixer'),
+	uglify = require ('gulp-uglify'),
+	sass = require ('gulp-sass'),
+	sourceMaps = require ('gulp-sourcemaps'),
+	rigger = require ('gulp-rigger'),
+	cssMin = require ('gulp-minify-css'),
+	rimRaf = require ('rimraf'),
+	browserSync = require ('browser-sync'),
+	reload = browserSync.reload;
+
+var path = {
+
+	build: {
+		html: 'build/',
+		js: 'build/js/',
+		css: 'build/css/'
+	},
+	src: {
+		html: 'src/*.html',
+		js: 'src/js/main.js',
+		style: 'src/style/main.scss'
+	},
+	watch: {
+		html: 'src/**/*.html',
+		js: 'src/js/**/*.js',
+		style: 'src/style/**/*.scss'
+	},
+	clean: './build'
+};
+
+gulp.task("webserver", function () {
+	browserSync({
+		server: {
+			baseDir: "./build"
+		},
+		host: 'localhost',
+		port: 3000,
+		tunnel: true
+	});
+});
+
+gulp.task('html:build', function () {
+	gulp.src(path.src.html)
+		.pipe (rigger())
+		.pipe (gulp.dest(path.build.html))
+		.pipe (reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+	gulp.src(path.src.js)
+		.pipe (rigger())
+		.pipe (sourcemaps.init())
+		.pipe (uglify())
+		.pipe (sourcemaps.write())
+		.pipe (gulp.dest(path.build.js))
+		.pipe (reload({stream: true}));
+});
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//
+// global libs
+//= ../../bower_components/jquery/dist/jquery.js
+/*
+ * Custom scripts
+ */
+//= partials/app.js
+//
